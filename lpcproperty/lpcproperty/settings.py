0e88bae1a0c02555c37e11960f857d644cce5292
erenroy <roypapan75@gmail.com>
2025-09-24 20:38:33 +0530

    add
    add

--- lpcproperty/lpcproperty/settings.py
+++ lpcproperty/lpcproperty/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a%hek)ut9v!n62a3-%m=)1ot6@8qf=@@+$k#4^81#6954c(p0j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
 
 # ALLOWED_HOSTS = ["backend.lotuspmc.com"]
 ALLOWED_HOSTS = ['72.60.123.152', 'localhost', '127.0.0.1' , 'backend.lotuspmc.com']
